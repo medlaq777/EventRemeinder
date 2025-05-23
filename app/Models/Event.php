1206661b39fc6e571c47c5f2d242e030474c85a5
medlaq777 <m.laqsoumi@gmail.com>
2025-05-23 19:03:07 +0100

    Update: Model Event With add Relations

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'event_date', 'organizer_id'];
+
+    public function recipients()
+    {
+        return $this->belongsToMany(Recipient::class)
+            ->withPivot(['reminder_sent', 'reminder_sent_at']);
+    }
+
+    public function organizer()
+    {
+        return $this->belongsTo(Recipient::class, 'organizer_id');
+    }
 }
