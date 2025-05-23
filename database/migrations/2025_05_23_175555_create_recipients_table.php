10416ef664ba3641f11b87e049241cdcfc425444
medlaq777 <m.laqsoumi@gmail.com>
2025-05-23 18:58:09 +0100

    Update: Schema for Recipient migration

--- database/migrations/2025_05_23_175555_create_recipients_table.php
+++ database/migrations/2025_05_23_175555_create_recipients_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('recipients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
