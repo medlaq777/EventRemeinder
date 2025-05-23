d2edccdc4e0b6ec68ec533071a6addd606b64a82
medlaq777 <m.laqsoumi@gmail.com>
2025-05-23 19:04:04 +0100

    Update: Model Recipient with make relations

--- app/Models/Recipient.php
+++ app/Models/Recipient.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipient extends Model
 {
-    //
+    protected $fillable = ['name', 'email', 'active'];
+
+    public function events()
+    {
+        return $this->belongsToMany(Event::class)
+            ->withPivot(['reminder_sent', 'reminder_sent_at']);
+    }
 }
