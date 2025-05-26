7f997454afd530b524ee6149d9943cf9badadb41
medlaq777 <m.laqsoumi@gmail.com>
2025-05-26 10:34:55 +0100

    Fix: fix the Queue Issues and now receive events emails

--- resources/views/emails/event_reminder.blade.php
+++ resources/views/emails/event_reminder.blade.php
@@ -1,17 +1,1 @@
-{{-- @component('mail::message')
-# Reminder: {{ $event->title }} is in 15 days!
-
-The event **'{{ $event->title }}'** is happening on {{ $event->start_date->format('F j, Y') }}.
-
-@component('mail::button', ['url' => url('/events/' . $event->id)])
-View Event
-@endcomponent
-
-Thank you for using our service!
-@endcomponent --}}
-
-
-
-
-
 test event reminder email
