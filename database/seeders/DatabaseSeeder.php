754e946c2aad5528f6975880428a2d3fbb9558b9
Miloud Ajouamy <ajouamymiloud@gmail.com>
2025-05-26 11:03:30 +0100

    Make: add event and recipient seeding to the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
+use App\Models\Recipient;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +21,37 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+
+        $event15 = Event::create([
+            'title' => 'Test Event',
+            'start_date' => now()->addDays(15),
+
+        ]);
+        $event10 = Event::create([
+            'title' => 'Test Event',
+            'start_date' => now()->addDays(10),
+
+        ]);
+        $event3 = Event::create([
+            'title' => 'Test Event',
+            'start_date' => now()->addDays(3),
+
+        ]);
+        $event1 = Event::create([
+            'title' => 'Test Event',
+            'start_date' => now()->addDays(1),
+
+        ]);
+
+        $recipient = Recipient::factory()->create([
+            'name' => 'Test Recipient',
+            'email' => 'ajouamymiloud@gmail.com',
+        ]);
+
+        $event15->recipients()->attach($recipient->id);
+        $event10->recipients()->attach($recipient->id);
+        $event3->recipients()->attach($recipient->id);
+        $event1->recipients()->attach($recipient->id);
     }
 }
